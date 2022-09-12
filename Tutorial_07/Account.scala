5c74ff4ba135748e1ec75ff9c3782142250cf6ee
SuraniDS <77562215+SuraniDS@users.noreply.github.com>
2022-09-13 01:20:14 +0530

    Update Account.scala

--- Tutorial_07/Account.scala
+++ Tutorial_07/Account.scala
@@ -1,16 +1,17 @@
-    class Account(nic:String, val accId: Int, var balance: Double = 0.0){
+    class Account(nic:String, val accId: Int, var accBalance: Double = 0.0){
 
         def withdrow(amount:Double) : Unit = {
-            this.balance = this.balance - amount
+            this.accBalance = this.accBalance - amount
         }
 
         def deposit(amount:Double) : Unit = {
-            this.balance = this.balance + amount
+            this.accBalance = this.accBalance + amount
         }
 
         def transfer(account:Int, amount:Double) : Unit = {
           val transferAcc = Q4.find(account, Q4.accountList)
-          if (balance < 0.0) println("Insufficient balance")
+
+          if (accaBalance < 0.0) println("Insufficient balance")
 
           else {
               this.withdrow(amount)
@@ -18,5 +19,5 @@
                 transferAcc(0).deposit(amount)
         }
 
-        override def toString = "["+nic+":"+accId +":"+ balance+"]"
+        override def toString = "["+nic+":"+accId +":"+ accBalance+"]"
     }
