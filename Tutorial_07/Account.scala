a743a75ca662d6e5f53621bac8c097fa92d0803c
SuraniDS <77562215+SuraniDS@users.noreply.github.com>
2022-09-13 08:23:24 +0530

    Update Account.scala

--- Tutorial_07/Account.scala
+++ Tutorial_07/Account.scala
@@ -1,23 +1,23 @@
     class Account(nic:String, val accId: Int, var accBalance: Double = 0.0){
 
-        def withdrow(amount:Double) : Unit = {
-            this.accBalance = this.accBalance - amount
-        }
+  def withdraw(amount:Double) : Unit = {
+    this.accBalance = this.accBalance - amount
+  }
 
-        def deposit(amount:Double) : Unit = {
-            this.accBalance = this.accBalance + amount
-        }
+  def deposit(amount:Double) : Unit = {
+    this.accBalance = this.accBalance + amount
+  }
 
-        def transfer(account:Int, amount:Double) : Unit = {
-          val transferAcc = Q4.find(account, Q4.accountList)
-
-          if (accaBalance < 0.0) println("Insufficient balance")
-
-          else {
-              this.withdrow(amount)
-            }
-                transferAcc(0).deposit(amount)
-        }
-
-        override def toString = "["+nic+":"+accId +":"+ accBalance+"]"
+  def transfer(account:Int, amount:Double) : Unit = {
+    val transferAcc = Q4.find(account, Q4.accountList)
+    if (accBalance < 0.0)
+      println("Your Account balance is not sufficient")
+    else {
+      this.withdraw(amount)
+      transferAcc(0).deposit(amount)
     }
+  }
+
+  override def toString = "["+nic+":"+accId +":"+ accBalance+"]"
+
+}
